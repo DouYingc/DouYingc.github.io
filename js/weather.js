3a7beea9be2559b0e7224aa8094fb5b96eb8dddb
DouYingc <1367358755@qq.com>
2022-07-26 22:31:14 +0800

    Site updated: 2022-07-26 22:31:14

--- js/weather.js
+++ js/weather.js
@@ -17,7 +17,7 @@ WIDGET = {
     "fixed": "true",
     "vertical": "center",
     "horizontal": "center",
-    "left": "160",
+    "left": "210",
     "top": "10",
     "key": "9b8cb870fbed433bb3126385564ede10"
   }
